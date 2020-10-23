998824d1a64bf8f080cd0cd652c88f64d6dfcea5
ellierk <ellierk@live.unc.edu>
2020-10-22 22:37:23 -0400

    quant reg tests

--- analysis script1.R
+++ analysis script1.R
@@ -1,18 +1,21 @@
-install.packages("dplyr")
-install.packages("ggplot2")
+
 library(dplyr)
 library(ggplot2)
+library(quantreg)
 
-squid.rawdata = read.csv("kremer csv squidpop data.csv", header = TRUE)
-summary(squid.data)
+squid.rawdata = read.csv("kremer consump assay data.csv", header = TRUE)
+summary(squid.rawdata)
 
-squid = squid.rawdata %>% mutate(consump = absent/reclaimed)
-
-plot(squid$dist_from_bags, squid$consump)
+squid = squid.rawdata %>% slice(6:65)
 
 squid %>% 
   ggplot(aes(x = dist_from_bags, y = consump, color = site)) + 
   geom_point() 
+
+
+boxplot(squid$consump)
+boxplot((squid%>% filter(site == "mera"))$consump)
+boxplot((squid%>% filter(site == "styron"))$consump)
 
 site.avgbydist = squid %>% 
     group_by(site, dist_from_bags)%>% 
@@ -21,3 +24,23 @@ site.avgbydist = squid %>%
 site.avgbydist %>% 
   ggplot(aes(x = dist_from_bags, y = avg_consump, color = site)) + 
   geom_point()
+
+# with outliers
+plot(density(logit(squid$consump)))
+
+#without outliers
+plot(density((squid %>% filter(consump < .7))$consump))
+
+#linreg 
+consumpByDist <- lm(consump ~ dist_from_bags, data = squid)
+AIC(consumpByDist)
+plot(resid(consumpByDist))
+
+# quantile regression attempts
+plot(squid$dist_from_bags, squid$consump)
+taus <- c(.05,.1,.25,.75,.90,.95)
+fit1 <- rq(consump ~ dist_from_bags, tau = .5, data = squid)
+abline(fit1)
+for(i in 1:length(taus)) {
+  abline(rq(consump ~ dist_from_bags, tau = taus[i], data = squid))
+}
