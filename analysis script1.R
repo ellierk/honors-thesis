d14d4696f774ed2c54ea7ee63e4648be6c2063d8
ellierk <ellierk@live.unc.edu>
2020-10-30 00:38:38 -0400

    graphs

--- analysis script1.R
+++ analysis script1.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(ggplot2)
 library(quantreg)
 library(quantreg)
+library(lme4)
 
 squid.rawdata = read.csv("kremer consump assay data.csv", header = TRUE)
 summary(squid.rawdata)
@@ -29,27 +30,64 @@ site.avgbydist %>%
 # with outliers
 plot(density(logit(squid$consump)))
 
-#without outliers
+#removing outliers
 Q <- quantile(squid$consump, probs=c(.25, .75), na.rm = FALSE)
 iqr <- IQR(squid$consump)
 up <-  Q[2]+1.5*iqr # Upper Range  
 low<- Q[1]-1.5*iqr # Lower Range
 squidElim<- subset(squid,
                    squid$consump > (Q[1] - 1.5*iqr) & squid$consump < (Q[2]+1.5*iqr))
+# data distribution without outliers
 plot(density(squidElim$consump))
+
+# avg point without outliers
+squid.sd = squidElim %>% 
+  group_by(site, dist_from_bags)%>% 
+  summarise(mean(consump), sd(consump)) %>% 
+  rename(avg_consump = 'mean(consump)', std_dev = 'sd(consump)') 
+  
+squid.sd %>% ggplot(aes(x = dist_from_bags, y = avg_consump, color = site)) + 
+  geom_point()
+
+# barplot
+squid.sd %>%
+  ggplot(aes(x = dist_from_bags, y = avg_consump, fill = site)) + 
+  geom_bar(stat = "identity", position = position_dodge()) + 
+  geom_errorbar(aes(ymin = avg_consump, ymax = avg_consump+std_dev), width=.2,
+                position=position_dodge(2)) +
+  xlab("Distance from bags (m)") + ylab("Average % Consumption") +
+  labs(title = "Average % Consumption of Squidpops by Lease and Treatment", )
+
+# dot plot without outliers
+ggplot(data = squidElim, aes(x=dist_from_bags, y = consump, col = site)) +
+  geom_point() + xlab("Distance from bags (m)") + ylab("% Consumption") + 
+  labs(title = "% Consumption by Distance")
+#+ geom_smooth(method = "lm", formula = y ~ x)
+
+# quantile regression 
+
+ggplot(data = squidElim, aes(x=dist_from_bags, y = consump)) +
+  geom_point() +geom_quantile(quantiles = c(.1, .25, .5, .75, .95)) +
+  xlab("Distance from bags (m)") + ylab("% Consumption") + 
+  labs(title = "% Consumption by Distance")
+
 
 #linreg 
 consumpByDist <- lm(consump ~ dist_from_bags, data = squidElim)
 AIC(consumpByDist)
 plot(resid(consumpByDist))
 
-# quantile regression attempts
-plot(squidElim$dist_from_bags, squidElim$consump)
-taus <- c(.05,.1,.25,.75,.90,.95)
-fit1 <- rq(consump ~ dist_from_bags, tau = .5, data = squidElim)
-abline(fit1)
-for(i in 1:length(taus)) {
-  abline(rq(consump ~ dist_from_bags, tau = taus[i], data = squidElim))
-}
-
 # glm model here
+
+consumpMulti <- glm(consump ~ dist_from_bags + site +
+                      temp_avg + sal_avg + 
+                      spacing_m + soak_start, family = binomial,
+                    data = squidElim)
+summary(consumpMulti)
+
+# glmer
+#consumpMixed <- glmer(consump ~ dist_from_bags + 
+ #                       temp_avg + sal_avg + 
+  #                      spacing_m + soak_start + (1|site),
+   #                   family = binomial,
+    #                  data = squidElim)
